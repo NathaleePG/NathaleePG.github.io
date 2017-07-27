9d7e1458f1d9da5c48d2f0f9483367888da252c9
NathaleePG <30455445+NathaleePG@users.noreply.github.com>
2017-07-27 16:22:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,12 +5,18 @@
 $(function() {
     var $conMain = $("#containerMain");
     var $header = $("header");
-    var $logo = $("#containerMain > a > img");
+    var $logo = $("#containerMain > img");
     var $footer = $("footer");
+    var $secCon = $("#containerSec");
     $conMain.hide().delay(200).fadeIn(1000);
     $header.hide().delay(1200).fadeIn(1500);
     $logo.hide().delay(3000).fadeIn(1500);
     $footer.hide().delay(4800).fadeIn(1500);
+
+    $logo.on("click", function(){
+        $conMain.fadeOut(1500);
+        $secCon.delay(1500).fadeIn(1500);
+    })
 
     /*
     function spin(item, delayTime){
