d8f81fee536028299d5d9f477a477a954cf45dc3
NathaleePG <30455445+NathaleePG@users.noreply.github.com>
2017-08-13 20:39:19 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -49,6 +49,19 @@ $(function() {
         }, 200);
     });
 
+    $("#containerSec > #fixed > ul:not(.icons) > li").on("click", function(){
+        $(this).find("ul").toggle();
+        $(this).find("li").toggle();
+
+        if($(this).find("ul").css("display") ==="none"){
+            $(this).css("opacity", 0.5);
+        } else{
+            $(this).css('opacity', 1);
+        }
+    });
+
+
+
 
 
 
