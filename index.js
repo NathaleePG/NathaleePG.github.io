76dd82b6597f305e34f1c00de966df40742a429b
NathaleePG <30455445+NathaleePG@users.noreply.github.com>
2017-08-17 01:21:19 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,25 @@
  */
 
 $(function() {
+    var $conMain = $("#containerMain");
+    var $header = $("header");
+    var $logo = $("#containerMain > img");
+    var $footer = $("footer");
     var $secCon = $("#containerSec");
     var $nav = $("#containerSec > #fixed > ul > li");
     var $bar = $("#containerSec > #fixed > ul");
     var $button = $("#toggle");
     var $body = $(".body");
     var $fixed = $("#containerSec > #fixed");
+    $conMain.delay(200).fadeIn(1000);
+    $header.hide().delay(1200).fadeIn(1500);
+    $logo.hide().delay(3000).fadeIn(1250);
+    $footer.hide().delay(4800).fadeIn(1250);
+
+    $logo.on("click", function(){
+        $conMain.fadeOut(1500);
+        $secCon.delay(1500).fadeIn(1500);
+    });
 
     $button.on("click", function(){
         $bar.toggle();
@@ -18,7 +31,6 @@ $(function() {
         } else{
             $button.css("opacity", 0.25);
             $fixed.css("border-bottom", "0");
-
         }
     });
 
@@ -48,35 +60,65 @@ $(function() {
         }
     });
 
-    function onResize() {
-        var w = $("#head");
-        if (w.outerWidth() >= $(window).width()) {
-            $("#value").css({
-                borderLeft: 0,
-                paddingLeft: 0,
-                marginRight: "20px"
-            });
-            w.width += '20px';
-            $("#key").css("border-bottom", "1px solid black");
-        }
-        else{
-            $("#value").css({
-                borderLeft: "1px solid black",
-                paddingLeft: "20px",
-                marginRight: 0,
-                marginLeft: "20px"
-            });
-            w.width -= '20px';
-            $("#key").css("border-bottom", 0);
+    function onResize(){
+        var target = $('#m1').offset();
+        if ($(window).scrollTop() > (target["top"] - 300)){
+            $("#m1").animate({
+                opacity:1
+            },3000)
         }
     }
-
-    onResize();
-
-    $(window).on("resize", function(){
+    $(window).on("scroll", function(){
         onResize();
     });
 
+    onResize();
+
+
+
+
+
+
+
+
+
+
+
+    /**
+    var $fb = $("#fb");
+    var $pin = $("#pin");
+    var $ig = $("#ig");
+
+    $fb.mouseenter(function(){
+        $(this).find(".resize").css("display", "none");
+        $(this).append("<img src='icon6.png' class='abs'>");
+    });
+    $fb.mouseleave(function(){
+        $(this).find(".abs").remove();
+        $(this).find(".resize").css("display", "block");
+
+    });
+
+    $pin.mouseenter(function(){
+        $(this).find(".resize").css("display", "none");
+        $(this).append("<img src='icon3.png' class='abs'>");
+    });
+    $pin.mouseleave(function(){
+        $(this).find(".abs").remove();
+        $(this).find(".resize").css("display", "block");
+
+    });
+
+    $ig.mouseenter(function(){
+        $(this).find(".resize").css("display", "none");
+        $(this).append("<img src='icon5.png' class='abs'>");
+    });
+    $ig.mouseleave(function(){
+        $(this).find(".abs").remove();
+        $(this).find(".resize").css("display", "block");
+
+    });
+    */
 
     /*
     $nav.on("mouseover", function(){
